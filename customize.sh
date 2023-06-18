47b1ff1759e27d02745745542fe081e442cf9513
jpus <78049266+jpus@users.noreply.github.com>
2023-06-19 02:36:58 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -13,4 +13,5 @@ sed -i 's/192.168.1.1/192.168.2.1/g' openwrt/package/base-files/files/bin/config
 
 #如需更改频率，请自行修改超频补丁第120行，1000Mhz=0x312 1100Mhz=0x362 1120Mhz=0x372 1200Mhz=0x3B2
 #复制内核5.10版本CPU超频补丁，如使用默频，将下方命令注释掉。
-#cp extra-files/322-mt7621-fix-cpu-clk-add-clkdev.patch target/linux/ramips/patches-5.10/
+cp extra-files/322-mt7621-fix-cpu-clk-add-clkdev.patch target/linux/ramips/patches-5.10/
+
