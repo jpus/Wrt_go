3dad092d98639c7380db99dc5ac0a43dbc2d28a3
jpus <78049266+jpus@users.noreply.github.com>
2023-06-21 14:59:38 +0800

    Add files via upload

---
+++ customize_noip6.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#=================================================
+# Description: DIY script
+#=================================================
+#1. Modify default IP
+sed -i 's/192.168.1.1/192.168.2.1/g' openwrt/package/base-files/files/bin/config_generate
+
+#2. 修改WIFI名称
+cp extra-files/mac80211.sh openwrt/package/kernel/mac80211/files/lib/wifi/mac80211.sh
+
+#3. 删除IPV6及相关配置防止编译报错
+cp extra-files/firewall/makefile openwrt/package/network/config/firewall/makefile
+cp extra-files/iptables/makefile openwrt/package/network/utils/iptables/makefile
+
+#如需更改频率，请自行修改超频补丁第120行，1000Mhz=0x312 1100Mhz=0x362 1120Mhz=0x372 1200Mhz=0x3B2
+#复制内核5.10版本CPU超频补丁，如使用默频，将下方命令注释掉。
+cp extra-files/322-mt7621-fix-cpu-clk-add-clkdev.patch openwrt/target/linux/ramips/patches-5.10/
+
