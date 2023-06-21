bfe1275bcca953fc09dc95ac2c16d47c54aa9a3d
jpus <78049266+jpus@users.noreply.github.com>
2023-06-22 04:25:47 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -5,8 +5,8 @@
 #1. Modify default IP
 sed -i 's/192.168.1.1/192.168.2.1/g' openwrt/package/base-files/files/bin/config_generate
 
-#2. Clear the login password
-#sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' openwrt/package/lean/default-settings/files/zzz-default-settings
+#2. 修改WIFI名称
+cp extra-files/mac80211.sh openwrt/package/kernel/mac80211/files/lib/wifi/
 
 #3. Replace with JerryKuKu’s Argon
 #rm openwrt/package/lean/luci-theme-argon -rf
